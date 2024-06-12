65b29c2560a436d1f7d3840b79357f7719167471
manjeetkumar10 <manjeetyadavmk9031@gmail.com>
2024-06-12 08:11:19 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,1 +1,38 @@
-manjeey
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract MyToken {
+    string public name;            // Token Name
+    string public symbol;          // Token Abbrv.
+    uint256 public totalSupply;    // Total Supply
+
+    mapping(address => uint256) public balances;
+
+    event Mint(address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+
+    constructor() {
+        name = "cryptocoin";           // Example Token Name
+        symbol = "CC";              // Example Token Abbreviation
+        totalSupply = 1000000;      // Example Initial Supply
+        balances[msg.sender] = totalSupply;
+    }
+
+    function mint(address _to, uint256 _value) public {
+        require(_value > 0, "Invalid mint value");
+        
+        totalSupply += _value;
+        balances[_to] += _value;
+        emit Mint(_to, _value);
+    }
+
+    function burn(uint256 _value) public {
+        require(_value > 0, "Invalid burn value");
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+
+        totalSupply -= _value;
+        balances[msg.sender] -= _value;
+        emit Burn(msg.sender, _value);
+    }
+}
+
